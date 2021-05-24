139b3b69e7520ae2bf5018b3ec518e435dedd1cd
FlamesLLC <84363801+FlamesLLC@users.noreply.github.com>
2021-05-24 13:54:24 -0700

    BELLA.AI [CONSOLE FIX HOTFIX] 52421 1:54 PM PST
    Just a patch

--- Bella.AI.py
+++ Bella.AI.py
@@ -2,16 +2,17 @@ print("Welcome to BELLA.AI  - Flamestopian AI Program")
  # WELCOME MESSAGE
 print("Welcome")  #  PRINTS WELCOME 
 ID = input("Please input your ID") # ID MESSAGE
+text = input("Enter text >")
 print("BELLA.AI Checking HAP-GEN 1.0")
 try:
     from happytransformer import HappyGeneration
 except:
     print("Import Error. Please 'pip install happytransformer' and try again.")
     input("> ")
-print("SCANNING..PLEASE ENTER TEXT TO GENERATOR.BELLA_AI BUILD 52021") # CHECKS GEN
+print("SCANNING..PLEASE ENTER TEXT TO GENERATOR.BELLA_AI BUILD 52421") # CHECKS GEN
 happy_gen = HappyGeneration("GPT-NEO", "EleutherAI/gpt-neo-1.3B") # GETS AI
 # Checks generator
-result = happy_gen.generate_text("Artificial intelligence program. Connnecting memory cores ")
+result = happy_gen.generate_text(text)
 print(result)
 print(result.text) # PRINTS RESULT
 print("BELLA: THANK YOU FOR USING BELLA.AI 1.0")
